c27288bdb260a401e30f5d394c9cbe2e16c5ac7f
Chukwuka Uba <promiseuba67@gmail.com>
2024-10-20 18:16:54 +0100

    fix prisma error with vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "output",
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-	webpack: (config) => {
-		config.externals.push("pino-pretty", "lokijs", "encoding");
-		return config;
-	},
+  // output: "output",
+  // eslint: {
+  // 	ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  // 	ignoreBuildErrors: true,
+  // },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/actions/transactionActions.ts
+++ src/actions/transactionActions.ts
@@ -4,6 +4,6 @@ export async function getUserTransactions(userId: string) {
   return prisma.transaction.findMany({
     where: { userId },
     orderBy: { createdAt: "desc" },
-    take: 10, // Limit to the 10 most recent transactions
+    take: 10,
   });
 }
