8429c059249f26d7c997ed64ea73e41983f6c9e8
Anioke sebastian <aniokechukwudi7@gmail.com>
2024-10-20 09:45:07 +0100

    style: update eslint configuration and ignoreDuringBuilds in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	webpack: (config) => {
+		config.externals.push("pino-pretty", "lokijs", "encoding");
+		return config;
+	},
 };
 
 export default nextConfig;
